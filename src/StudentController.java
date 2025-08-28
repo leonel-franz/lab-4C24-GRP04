b4b39ef79279e551a793eebb3251f102e44eb853
Leonel Portillo <leonel.portillo@tecsup.edu.pe>
2025-08-27 23:16:16 -0500

    Mejoras en StudentController y StudentReport (sprint-2)

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,5 +1,11 @@
 public class StudentController {
     public static void main(String[] args) {
         System.out.println("Bienvenido al sistema de estudiantes.");
+        System.out.println("Cargando datos del estudiante...");
+
+        StudentReport reporte = new StudentReport();
+        reporte.generarReporte();
+
+        System.out.println("Sistema finalizado correctamente.");
     }
 }
--- src/StudentReport.java
+++ src/StudentReport.java
@@ -1,5 +1,6 @@
 public class StudentReport {
     public void generarReporte() {
         System.out.println("Generando reporte de estudiantes...");
+        System.out.println("Reporte generado exitosamente: [Total de estudiantes: 25]");
     }
 }
